6c91217c7a2ff33cb09f135e1246d8f23cbfafaa
Planky <k.planky@proton.me>
2025-03-29 01:43:45 +0700

    Added Prediction

--- pong.py
+++ pong.py
@@ -7,32 +7,52 @@ gymnasium.register_envs(ale_py)
 # https://ale.farama.org/environments/pong/
 
 
+def get_ball_loc(img):
+    _ball_xy = np.where(img[34:194, 20:140] == 236)
+    if len(_ball_xy[0]):  # Check if empty
+        ball_x = np.mean(_ball_xy[1]) + 20  # x
+        ball_y = np.mean(_ball_xy[0]) + 34  # y
+        return (ball_x, ball_y)
+    return (0, 0)
+
+
 env = gymnasium.make(
     "ALE/Pong-v5",
     obs_type="grayscale",
     # render_mode="human"
 )
-
-
 env.reset()
 score = 0
-for frame in range(15):  # 15
+
+target_frame = 35
+last_ball = (0, 0)
+
+for frame in range(target_frame):  # 15
     action = env.action_space.sample()
 
     img, reward, terminated, truncated, info = env.step(action)
     score += int(reward)
 
-    if frame > 13:  # frame > 13
+    if frame > 26:  # frame > 13
         plt.imshow(img)
 
         paddle_y = np.mean(np.where(img[34:194, 141] == 147)) + 34
-        plt.plot(141, paddle_y, "xr")
 
-        _ball_xy = np.where(img[34:194, 20:140] == 236)
-        ball_x = np.mean(_ball_xy[1]) + 20  # x
-        ball_y = np.mean(_ball_xy[0]) + 34  # y
-        plt.plot(ball_x, ball_y, "xr")
+        ball = get_ball_loc(img)
 
+        if last_ball[0] == 0 or ball[0] == 0:
+            last_ball = ball
+            continue
+
+        plt.plot(141.5, paddle_y, "xr")
+        plt.plot(last_ball[0], last_ball[1], "xr")
+
+        print(ball, last_ball, paddle_y)
+
+        displacement = np.subtract(ball, last_ball)
+        print(displacement)
+
+        plt.plot(ball[0] + displacement[0], ball[1] + displacement[1], "xg")
         # Top bottom border Y
         # plt.axhline(34, color="red")
         # plt.axhline(193, color="red")
@@ -41,6 +61,7 @@ for frame in range(15):  # 15
         # plt.axvline(20, color="red")
         # plt.axvline(139, color="red")
 
+        last_ball = ball
         plt.show()
 
     if terminated or truncated:
